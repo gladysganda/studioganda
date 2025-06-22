fe1c696e4fc11ecec3b24ae0f75db23a34d5d365
Gladys Ganda <gladysganda8@gmail.com>
2025-06-22 16:00:02 +0700

    Add disabled save image function on site

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,32 @@
 // app/layout.js
+
+"use client";
+import { useEffect } from "react";
 import '../styles/globals.css';
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const handleContextMenu = (e) => {
+      e.preventDefault();
+    };
+    document.addEventListener("contextmenu", handleContextMenu);
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+
+
   return (
     <html lang="en">
+    <head>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="apple-touch-fullscreen" content="yes" />
+      <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
+    </head>
       <body>
         <Navbar />
         <main>{children}</main>
